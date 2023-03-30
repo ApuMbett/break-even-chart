c0044aecc71b60442e11c1c2aa78eec0c2aeb490
ApuMbett <105366684+ApuMbett@users.noreply.github.com>
2023-03-30 16:52:15 +0200

    implemented BreakEvenChart component

--- src/BreakEvenChart.jsx
+++ src/BreakEvenChart.jsx
@@ -1,22 +1,86 @@
-import { Chart } from "react-google-charts";
-
+import { Chart, GoogleDataTableColumnRoleType } from "react-google-charts";
+function calculateBreakEvenPoint(salesPrice, variableCosts, fixedCosts) {
+  return fixedCosts / (salesPrice - variableCosts);
+}
 export default function BreakEvenChart(props) {
-  options = {
+  const BEP = calculateBreakEvenPoint(
+    props.salesPrice,
+    props.variableCosts,
+    props.fixedCosts
+  );
+  const SCALE = 2; //how many times the BEP units you want to show after BEP
+  let data = [
+    [
+      "X",
+      "MASK",
+      "Loss Area",
+      "Profit Area",
+      "Total Costs",
+      "Revenues",
+      { type: "string", role: "style" },
+    ],
+    [0, props.fixedCosts, -props.fixedCosts, null, props.fixedCosts, 0, null], //assuming that before bep you are in loss and so revenues function is always starting from 0
+    [
+      BEP,
+      props.variableCosts * BEP + props.fixedCosts,
+      props.salesPrice * BEP - (props.variableCosts * BEP + props.fixedCosts),
+      props.salesPrice * BEP - (props.variableCosts * BEP + props.fixedCosts),
+      props.variableCosts * BEP + props.fixedCosts,
+      props.salesPrice * BEP,
+      "point { size: 8; shape-type: diamond; fill-color: #0ff; visible: true }",
+    ], //BREAK EVEN POINT
+    [
+      BEP * SCALE,
+
+      props.variableCosts * BEP * SCALE + props.fixedCosts,
+      null,
+      props.salesPrice * BEP * SCALE -
+        (props.variableCosts * BEP * SCALE + props.fixedCosts),
+      props.variableCosts * BEP * SCALE + props.fixedCosts,
+      props.salesPrice * BEP * SCALE,
+      null,
+    ],
+  ];
+  const options = {
     title: "Break Even Chart",
     curveType: "function",
-    legend: { position: "bottom" },
-    hAxis: { gridlines: { count: 5 } },
+    legend: { position: "right" },
+    // hAxis: { gridlines: { count: 5 } },
     enableInteractivity: false,
-    explorer: {},
+    //explorer: {}, //TODO prevent negative quadrants
+    isStacked: true,
+    series: {
+      0: {
+        type: "area",
+        color: "transparent",
+        visibleInLegend: false,
+      },
+      1: {
+        type: "area",
+        color: "red",
+      },
+      2: {
+        type: "area",
+        color: "green",
+      },
+      3: {
+        type: "line",
+        color: "orange",
+      },
+      4: {
+        type: "line",
+        color: "blue",
+      },
+    },
   };
-  data = {};
+
   return (
     <Chart
-      chartType="LineChart"
+      chartType="ComboChart"
       data={data}
       width={props.width}
       height={props.height}
-      options={props.options ?? options}
+      options={options} //{props.options ?? options} //TODO add style capabilities
     />
   );
 }
